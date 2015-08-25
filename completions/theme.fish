816f4acf79d8e3a046df5679a890818e4f0241e2
Don <don@ohspite.net>
2015-08-25 14:06:11 -0400

    Fix bug preventing theme auto-completion.
    Also, prevent files in the current directory from being shown in the
    completion list.

--- completions/theme.fish
+++ completions/theme.fish
@@ -1,4 +1,4 @@
 # Add completions when the theme is loaded. Thanks Bruno!
 complete --command (basename -s.fish (status -f)) \
-  --arguments "(basename (theme.util.get.themes))" \
-  --description "Oh-my-fish theme"
+  --arguments "(basename -a (theme.util.get.themes))" \
+  --description "Oh-my-fish theme" --no-files
